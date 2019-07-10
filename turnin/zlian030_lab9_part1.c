b0234af43231cf79f474050ce7b504ee8e2861b8
zhangcheng liang <zlian030@wch136-21.cs.ucr.edu>
2019-07-10 14:38:19 -0700

    all

--- turnin/zlian030_lab9_part1.c
+++ turnin/zlian030_lab9_part1.c
@@ -8,61 +8,17 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>
 //#ifdef _SIMULATE_
 //#include "simAVRHeader.h"
 //#endif
 
-enum State{start, on, off} state;
+enum State{start, sounds, off} state;
 unsigned char button1;
 unsigned char button2;
 unsigned char button3;
 
-unsigned char current = 0x00;
-volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
 
-void TimerOn() {
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
-}
 
-void TimerOff() {
-	TCCR1B = 0x00;
-}
-
-void TimerISR() {
-	TimerFlag = 1;
-}
-
-ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0) {
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-double melody[] = {2616.3, 0.0, 2616.3, 0.0, 2936.6, 0.0, 3296.3, 0.0, 2616.3, 0.0, 3296.3, 0.0, 2936.6, 0.0, 2616.3, 0.0, 2616.3, 0.0};
-/*										2616.3); }
-			if (current == 0x01) { set_PWM(2936.6); }
-			if (current == 0x02) { set_PWM(3296.3); }
-			if (current == 0x03) { set_PWM(3492.3); }
-			if (current == 0x04) { set_PWM(3920.0); }
-			if (current == 0x05) { set_PWM(4400.0); }
-			if (current == 0x06) { set_PWM(4938.8); }
-			if (current == 0x07) { set_PWM(5232.5);
-*/
 void set_PWM(double frequency) {
 	static double current_frequency;
 	if (frequency != current_frequency) {
@@ -91,21 +47,24 @@ void PWM_off() {
 
 void tick() {
 	button1 = ~PINA & 0x01;
+	button2 = ~PINA & 0x02;
+	button3 = ~PINA & 0x04;
 	switch(state) {
 		case start:
 			state = off;
 			break;
-		case on:
-
-			if (current == 18) {
+		case sounds:
+			if ((button1 && !button2 && !button3) || (!button1 && button2 && !button3) || (!button1 && !button2 && button3)) {
+				state = sounds;
+			}
+			else {
 				state = off;
 			}
-			else {}
 			break;
 			
 		case off:
-			if (button1 && !button2 && !button3) {
-				state = on;
+			if ((button1 && !button2 && !button3) || (!button1 && button2 && !button3) || (!button1 && !button2 && button3)) {
+				state = sounds;
 			}
 			else {
 				state = off;
@@ -119,17 +78,14 @@ void tick() {
 		case start:
 			state = off;
 			break;
-			
-		case on:
-			if (current < 18) {
-				set_PWM(melody[current]);
-				current++;
-			}			
+		case sounds:
+			if (button1 && !button2 && !button3) { set_PWM(2616.3); }
+			if (!button1 && button2 && !button3) { set_PWM(2936.6); }
+			if (!button1 && !button2 && button3) { set_PWM(3296.3); }
 			break;
 			
 		case off:
 			set_PWM(0);
-			current = 0;
 			break;
 			
 		default:
@@ -142,13 +98,10 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
 	PWM_on();
-	TimerSet(38);
-    TimerOn();
     /* Insert your solution below */
     while (1) {
+		
 		tick();
-		while (!TimerFlag);
-		TimerFlag = 0;
     }
     return 1;
 }
--- turnin/zlian030_lab9_part2.c
+++ turnin/zlian030_lab9_part2.c
@@ -8,7 +8,6 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>
 //#ifdef _SIMULATE_
 //#include "simAVRHeader.h"
 //#endif
@@ -17,52 +16,9 @@ enum State{start, on, off} state;
 unsigned char button1;
 unsigned char button2;
 unsigned char button3;
-
+unsigned char flag = 0x00;
 unsigned char current = 0x00;
-volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
 
-void TimerOn() {
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
-}
-
-void TimerOff() {
-	TCCR1B = 0x00;
-}
-
-void TimerISR() {
-	TimerFlag = 1;
-}
-
-ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0) {
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-double melody[] = {2616.3, 0.0, 2616.3, 0.0, 2936.6, 0.0, 3296.3, 0.0, 2616.3, 0.0, 3296.3, 0.0, 2936.6, 0.0, 2616.3, 0.0, 2616.3, 0.0};
-/*										2616.3); }
-			if (current == 0x01) { set_PWM(2936.6); }
-			if (current == 0x02) { set_PWM(3296.3); }
-			if (current == 0x03) { set_PWM(3492.3); }
-			if (current == 0x04) { set_PWM(3920.0); }
-			if (current == 0x05) { set_PWM(4400.0); }
-			if (current == 0x06) { set_PWM(4938.8); }
-			if (current == 0x07) { set_PWM(5232.5);
-*/
 void set_PWM(double frequency) {
 	static double current_frequency;
 	if (frequency != current_frequency) {
@@ -91,21 +47,36 @@ void PWM_off() {
 
 void tick() {
 	button1 = ~PINA & 0x01;
+	button2 = ~PINA & 0x02;
+	button3 = ~PINA & 0x04;
 	switch(state) {
 		case start:
 			state = off;
 			break;
 		case on:
-
-			if (current == 18) {
+			if (button1 && !button2 && !button3 && flag == 0x01){
+				if (current != 7) { current++; }
+				flag = 0x00;
+			}
+			else if (!button1 && button2 && !button3 && flag == 0x01){
+				if (current != 0) { current--; }
+				flag = 0x00;
+			}
+			else if (!button1 && !button2 && !button3) {
+				flag = 0x01;
+			}
+			else if (!button1 && !button2 && button3 && flag == 0x01) {
 				state = off;
 			}
 			else {}
 			break;
 			
 		case off:
-			if (button1 && !button2 && !button3) {
+			if (!button1 && !button2 && button3 && flag == 0x00) {
 				state = on;
+			}
+			else if (!button1 && !button2 && !button3 && flag == 0x01) {
+				flag = 0x00;
 			}
 			else {
 				state = off;
@@ -121,15 +92,18 @@ void tick() {
 			break;
 			
 		case on:
-			if (current < 18) {
-				set_PWM(melody[current]);
-				current++;
-			}			
+			if (current == 0x00) { set_PWM(2616.3); }
+			if (current == 0x01) { set_PWM(2936.6); }
+			if (current == 0x02) { set_PWM(3296.3); }
+			if (current == 0x03) { set_PWM(3492.3); }
+			if (current == 0x04) { set_PWM(3920.0); }
+			if (current == 0x05) { set_PWM(4400.0); }
+			if (current == 0x06) { set_PWM(4938.8); }
+			if (current == 0x07) { set_PWM(5232.5); }
 			break;
 			
 		case off:
 			set_PWM(0);
-			current = 0;
 			break;
 			
 		default:
@@ -142,13 +116,9 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
 	PWM_on();
-	TimerSet(38);
-    TimerOn();
     /* Insert your solution below */
     while (1) {
 		tick();
-		while (!TimerFlag);
-		TimerFlag = 0;
     }
     return 1;
 }
